71c578f44e02497f5d02f5e82b0ad27f24d87274
chayansrk64 <chayansrk64@gmail.com>
2025-08-22 22:49:01 +0600

    styling elements

--- app.js
+++ app.js
@@ -14,3 +14,16 @@
 
 // querySelectorAll()
 
+
+// styling elements
+const title =  document.querySelector('#title');
+title.style.color = 'red';
+// console.log(title)
+
+const listItems =  document.querySelectorAll('.list-items')
+for(let item of listItems){
+    item.style.fontSize = '2rem'
+}
+// console.log(listItems)
+
+
