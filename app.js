2229d2fb09bd9f63fcc0c397512f64d3705ca0af
chayansrk64 <chayansrk64@gmail.com>
2025-08-22 23:33:14 +0600

    creating and modifing elements

--- app.js
+++ app.js
@@ -15,15 +15,51 @@
 // querySelectorAll()
 
 
-// styling elements
-const title =  document.querySelector('#title');
-title.style.color = 'red';
+// styling elements ============
+
+// const title =  document.querySelector('#title');
+// title.style.color = 'red';
 // console.log(title)
 
-const listItems =  document.querySelectorAll('.list-items')
-for(let item of listItems){
-    item.style.fontSize = '2rem'
-}
+// const listItems =  document.querySelectorAll('.list-items')
+// for(let item of listItems){
+//     item.style.fontSize = '2rem'
+// }
 // console.log(listItems)
+
+
+
+// creating elements ==============
+
+const ul = document.querySelector('ul');
+const li = document.createElement('li')
+
+// adding elements
+ul.append(li)
+
+// modifying the text
+li.innerText = 'X-men'
+
+// const firstListItem = document.querySelector('.list-items')
+// console.log(firstListItem.innerText)
+// console.log(firstListItem.textContent)
+// console.log(firstListItem.innerHTML)
+
+
+// modifying attributes and classes
+
+// li.setAttribute('id', 'title');
+// li.removeAttribute('id');
+// let title = document.querySelector('#title');
+// title.getAttribute('id')
+
+
+li.classList.add('list-items');
+li.classList.remove('list-items');
+
+// console.log(li.classList.contains('list-items')) 
+
+// remove elements
+li.remove()
 
 
