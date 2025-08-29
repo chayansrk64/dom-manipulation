89c51414bc97daecf8553d9ac6a9801284c91287
chayansrk64 <chayansrk64@gmail.com>
2025-08-29 09:45:05 +0600

    accordion modified

--- accordion/accordion.js
+++ accordion/accordion.js
@@ -2,6 +2,13 @@ const accordion = document.getElementsByClassName('content-container');
 
 for(let item of accordion){
     item.addEventListener('click', function(){
+        // first, close all other accordian items
+        for(let otherItem of accordion){
+            if(otherItem !== this){
+                otherItem.classList.remove('active')
+            }
+        }
+        // then, toggle 'active' class on the clicked item
         this.classList.toggle('active');
     })
 }
